e8b8c14d36c46447d5dd1372438b21640d7c6d96
DarkCrunch <josuelopezquijandria@gmail.com>
2020-11-01 09:40:45 -0500

    Initial commit

---
+++ Java001Comentarios/src/java001comentarios/Java001Comentarios.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java001comentarios;
+
+/**
+ *
+ * @author DARKCRUNCH
+ */
+public class Java001Comentarios {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*Como podemos ver, en la clase principal por defecto existen dos tipos
+        de comentarios los que son de una sola linea y los que son de multiple
+        linea*/
+        
+        //Este es un comentario de una linea
+        
+        /*
+        Este es un comentario multilinea
+        */
+        
+        /*Los comentarios nos sirven para describir las definiciones, procesos,
+        atributos, clases, constructores, etc. Para poder leer y entender el
+        codigo de una manera sencilla, precisa y comprensible*/
+    }
+    
+}
