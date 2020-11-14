b418c5b124a8468e9652adaf46e171a579b5b5c7
DarkCrunch <josuelopezquijandria@gmail.com>
2020-11-14 01:59:27 -0500

    java

--- Java005Condicionales/src/java005condicionales/Java005Condicionales.java
+++ Java005Condicionales/src/java005condicionales/Java005Condicionales.java
@@ -15,7 +15,23 @@ public class Java005Condicionales {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        /*
+        SENTENCIAS DE CONTROL DEL FLUJO DE UN PROGRAMA
+        Cuando se escribe un programa, se introduce la secuencia de sentencias
+        dentro de un archivo. Sin sentencias de control del flujo, el interprete
+        ejecuta las sentencias conforme aparecen en el programa de principio a 
+        fin. Las sentecias de control de flujo se emplean en los programas para
+        ejecutar sentencias condicionalmente, repetir un conjunto de sentencias
+        o, en general, cambiar el flujo secuencial de ejecucion. Las sentencias
+        selectivas o condicionales se veran en este capitulo y las sentencias
+        repetitivas en el siguiente.
+        
+        SENTENCIA IF - ELSE
+        Es una bifurcacion o sentencia condicional de una o dos ramas. La
+        sentencia de control evalua la condicion logica o booleana. Si esta
+        condicion es cierta entonces se ejecuta la sentencia o sentencias(1) que
+        se
+        */
     }
     
 }
