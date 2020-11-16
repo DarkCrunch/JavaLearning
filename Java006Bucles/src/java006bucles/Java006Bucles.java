14c5357e4f96e50b37632bfb970a0458942c4ed7
DarkCrunch <josuelopezquijandria@gmail.com>
2020-11-15 22:18:03 -0500

    java

---
+++ Java006Bucles/src/java006bucles/Java006Bucles.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java006bucles;
+
+/**
+ *
+ * @author DARKCRUNCH
+ */
+public class Java006Bucles {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*
+        SENTENCIAS REPETITIVAS O BUCLES
+        
+        */
+    }
+    
+}
